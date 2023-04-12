c5d760782896ab147f161fccca7a8296768d1565
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-12 12:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(event) {
+      // This function will be called when the DOM is loaded
+      var body = document.getElementsByTagName("body")[0];
+      body.innerText = "DOM load success";
+    });
